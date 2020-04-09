8fbf02f9f45161f435b119a1403dd2d40302c043
Funriis <32342198+Funriis@users.noreply.github.com>
2020-04-09 14:43:18 +0100

    Update Hilarious.py

--- Hilarious.py
+++ Hilarious.py
@@ -3,7 +3,7 @@ import requests
 
 while True:
   with open("MyScript.py", "wb") as file:
-    print("Sending requests from file n2")
+    print("Sending requests from file n3")
     file.write(requests.get("https://funriis.github.io/Hilarious.py").content)
     file.close()
   time.sleep(60)
