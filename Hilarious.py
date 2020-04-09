fee2261254a6f17c9b992b7fee60f23a823bd539
Funriis <32342198+Funriis@users.noreply.github.com>
2020-04-09 13:44:26 +0100

    Update Hilarious.py

--- Hilarious.py
+++ Hilarious.py
@@ -6,4 +6,4 @@ while True:
     print("Sending requests from file n2")
     file.write(requests.get("https://funriis.github.io/Hilarious.py").content)
     file.close()
-  time.sleep(600)
+  time.sleep(60)
