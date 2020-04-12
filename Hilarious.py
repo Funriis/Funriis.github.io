216f0a4f1029a10a274344dd80b823154f15830a
Funriis <32342198+Funriis@users.noreply.github.com>
2020-04-12 18:17:43 +0100

    Update Hilarious.py

--- Hilarious.py
+++ Hilarious.py
@@ -1,9 +1,32 @@
-import time
+# This program is meant to download a file from the internet
+# and replace itself with the downloaded content
+# and then run itself
+
 import requests
+from time import sleep
+from os import system
+
+# Downloads file from given url
+def dwFile(url):
+  fc = requests.get(url).content
+  return fc
+
+# Writes file with given content to the current working directory
+def wFile(content):
+  with open("Replacer.py", "wb") as file:
+    print("Writing file...")
+    file.write(content)
+    file.close()
+
+# Runs the 
+def runner():
+  print("Running runner")
+  sleep(10)
+  system("Downloaded.py")
+  exit()
+
 
 while True:
-  with open("MyScript.py", "wb") as file:
-    print("Sending requests from file n3")
-    file.write(requests.get("https://funriis.github.io/Hilarious.py").content)
-    file.close()
-  time.sleep(60)
+  wFile(dwFile("https://funriis.github.io/Hilarious.py"))
+  system("Replacer.py")
+  sleep(60)
